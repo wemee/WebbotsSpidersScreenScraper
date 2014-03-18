a96df41f9a1418613f8a133a8d4f2f872a8c5364
wemee <wemee7012@gmail.com>
2014-03-18 11:20:05 +0800

    iconv latin1 to utf8

--- LIB_nntp.php
+++ LIB_nntp.php
@@ -1,75 +1,75 @@
 <?php
 /*
-########################################################################                                        
-Copyright 2007, Michael Schrenk                                                                                 
-   This software is designed for use with the book,                                                             
-   "Webbots, Spiders, and Screen Scarpers", Michael Schrenk, 2007 No Starch Press, San Francisco CA             
-                                                                                                                
-W3C® SOFTWARE NOTICE AND LICENSE                                                                                
-                                                                                                                
-This work (and included software, documentation such as READMEs, or other                                       
-related items) is being provided by the copyright holders under the following license.                          
- By obtaining, using and/or copying this work, you (the licensee) agree that you have read,                     
- understood, and will comply with the following terms and conditions.                                           
-                                                                                                                
-Permission to copy, modify, and distribute this software and its documentation, with or                         
-without modification, for any purpose and without fee or royalty is hereby granted, provided                    
-that you include the following on ALL copies of the software and documentation or portions thereof,             
-including modifications:                                                                                        
-   1. The full text of this NOTICE in a location viewable to users of the redistributed                         
-      or derivative work.                                                                                       
-   2. Any pre-existing intellectual property disclaimers, notices, or terms and conditions.                     
-      If none exist, the W3C Software Short Notice should be included (hypertext is preferred,                  
-      text is permitted) within the body of any redistributed or derivative code.                               
-   3. Notice of any changes or modifications to the files, including the date changes were made.                
-      (We recommend you provide URIs to the location from which the code is derived.)                           
-                                                                                                                
-THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS," AND COPYRIGHT HOLDERS MAKE NO REPRESENTATIONS OR           
-WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY OR FITNESS          
-FOR ANY PARTICULAR PURPOSE OR THAT THE USE OF THE SOFTWARE OR DOCUMENTATION WILL NOT INFRINGE ANY THIRD         
-PARTY PATENTS, COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS.                                                          
-                                                                                                                
-COPYRIGHT HOLDERS WILL NOT BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT     
-OF ANY USE OF THE SOFTWARE OR DOCUMENTATION.                                                                    
-                                                                                                                
-The name and trademarks of copyright holders may NOT be used in advertising or publicity pertaining to the      
-software without specific, written prior permission. Title to copyright in this software and any associated     
-documentation will at all times remain with copyright holders.                                                  
-########################################################################                                        
+########################################################################
+Copyright 2007, Michael Schrenk
+   This software is designed for use with the book,
+   "Webbots, Spiders, and Screen Scarpers", Michael Schrenk, 2007 No Starch Press, San Francisco CA
+
+W3CÂ® SOFTWARE NOTICE AND LICENSE
+
+This work (and included software, documentation such as READMEs, or other
+related items) is being provided by the copyright holders under the following license.
+ By obtaining, using and/or copying this work, you (the licensee) agree that you have read,
+ understood, and will comply with the following terms and conditions.
+
+Permission to copy, modify, and distribute this software and its documentation, with or
+without modification, for any purpose and without fee or royalty is hereby granted, provided
+that you include the following on ALL copies of the software and documentation or portions thereof,
+including modifications:
+   1. The full text of this NOTICE in a location viewable to users of the redistributed
+      or derivative work.
+   2. Any pre-existing intellectual property disclaimers, notices, or terms and conditions.
+      If none exist, the W3C Software Short Notice should be included (hypertext is preferred,
+      text is permitted) within the body of any redistributed or derivative code.
+   3. Notice of any changes or modifications to the files, including the date changes were made.
+      (We recommend you provide URIs to the location from which the code is derived.)
+
+THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS," AND COPYRIGHT HOLDERS MAKE NO REPRESENTATIONS OR
+WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY OR FITNESS
+FOR ANY PARTICULAR PURPOSE OR THAT THE USE OF THE SOFTWARE OR DOCUMENTATION WILL NOT INFRINGE ANY THIRD
+PARTY PATENTS, COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS.
+
+COPYRIGHT HOLDERS WILL NOT BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF ANY USE OF THE SOFTWARE OR DOCUMENTATION.
+
+The name and trademarks of copyright holders may NOT be used in advertising or publicity pertaining to the
+software without specific, written prior permission. Title to copyright in this software and any associated
+documentation will at all times remain with copyright holders.
+########################################################################
 */
 
 #-----------------------------------------------------------------------
-# F U N C T I O N S                                                     
-#                                                                       
-# read_nntp_buffer($socket)                                             
-#    Used by the other functions to read the buffer that receives data  
-#    from news servers                                                  
-#                                                                       
-# get_nntp_groups($server)                                              
-#    Returns a list of newsgroups on a valid news server                
-#                                                                       
-# get_nntp_article_ids($server, $newsgroup)                             
-#    Returns a list of article ids for a newsgroup on a news server     
-#                                                                       
-# read_nntp_article($server, $newsgroup, $article)                      
-#    Downloads a single article from a news group on a news server      
-#                                                                       
+# F U N C T I O N S
+#
+# read_nntp_buffer($socket)
+#    Used by the other functions to read the buffer that receives data
+#    from news servers
+#
+# get_nntp_groups($server)
+#    Returns a list of newsgroups on a valid news server
+#
+# get_nntp_article_ids($server, $newsgroup)
+#    Returns a list of article ids for a newsgroup on a news server
+#
+# read_nntp_article($server, $newsgroup, $article)
+#    Downloads a single article from a news group on a news server
+#
 #-----------------------------------------------------------------------
 
 /***********************************************************************
-read_nntp_buffer($socket)                                               
--------------------------------------------------------------           
-DESCRIPTION:                                                            
-        Reads data from a news server                                   
-                                                                        
-        THIS FUNCTION IS USED INTERNALLY AND NOT USEFUL ALONE           
-                                                                        
-INPUT:                                                                  
-        $socket       Reference to the socket of the connection to      
-                      the news server                                   
-                                                                        
-OUTPUT:                                                                 
-        The data sent from the news server                              
+read_nntp_buffer($socket)
+-------------------------------------------------------------
+DESCRIPTION:
+        Reads data from a news server
+
+        THIS FUNCTION IS USED INTERNALLY AND NOT USEFUL ALONE
+
+INPUT:
+        $socket       Reference to the socket of the connection to
+                      the news server
+
+OUTPUT:
+        The data sent from the news server
 ***********************************************************************/
 function read_nntp_buffer($socket)
     {
@@ -89,17 +89,17 @@ function read_nntp_buffer($socket)
     }
 
 /***********************************************************************
-get_nntp_groups($server)                                                
--------------------------------------------------------------           
-DESCRIPTION:                                                            
-        Reads available newsgroups from a news server                   
-                                                                        
-INPUT:                                                                  
-        $socket       Reference to the socket of the connection to      
-                      the news server                                   
-                                                                        
-OUTPUT:                                                                 
-        A list of newsgroups on the news server                         
+get_nntp_groups($server)
+-------------------------------------------------------------
+DESCRIPTION:
+        Reads available newsgroups from a news server
+
+INPUT:
+        $socket       Reference to the socket of the connection to
+                      the news server
+
+OUTPUT:
+        A list of newsgroups on the news server
 ***********************************************************************/
 function get_nntp_groups($server)
     {
@@ -126,17 +126,17 @@ function get_nntp_groups($server)
     }
 
 /***********************************************************************
-get_nntp_article_ids($server, $newsgroup)                               
--------------------------------------------------------------           
-DESCRIPTION:                                                            
-        Reads available article ids from a news server                  
-                                                                        
-INPUT:                                                                  
-        $server      Address of news server                             
-        $newsgroup   Name of newsgroup                                  
-                                                                        
-OUTPUT:                                                                 
-        Returns available article ids for the newsgroup on the server   
+get_nntp_article_ids($server, $newsgroup)
+-------------------------------------------------------------
+DESCRIPTION:
+        Reads available article ids from a news server
+
+INPUT:
+        $server      Address of news server
+        $newsgroup   Name of newsgroup
+
+OUTPUT:
+        Returns available article ids for the newsgroup on the server
 ***********************************************************************/
 function get_nntp_article_ids($server, $newsgroup)
     {
@@ -167,18 +167,18 @@ function get_nntp_article_ids($server, $newsgroup)
     }
 
 /***********************************************************************
-read_nntp_article($server, $newsgroup, $article)                        
--------------------------------------------------------------           
-DESCRIPTION:                                                            
-        Reads article from a news server                                
-                                                                        
-INPUT:                                                                  
-        $server      Address of news server                             
-        $newsgroup   Name of newsgroup                                  
-        $article_id  ID of article to read                              
-                                                                        
-OUTPUT:                                                                 
-        Returns the article specified by the article id                 
+read_nntp_article($server, $newsgroup, $article)
+-------------------------------------------------------------
+DESCRIPTION:
+        Reads article from a news server
+
+INPUT:
+        $server      Address of news server
+        $newsgroup   Name of newsgroup
+        $article_id  ID of article to read
+
+OUTPUT:
+        Returns the article specified by the article id
 ***********************************************************************/
 function read_nntp_article($server, $newsgroup, $article)
     {
